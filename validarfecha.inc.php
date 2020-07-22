0d207f751ecd59c3d5ef105ae18e0a8cf8fa99e6
Alvaro VP <jhonalvarovallejosperez@gmail.com>
2020-07-22 11:15:15 -0400

    Update validarfecha.inc.php

--- validarfecha.inc.php
+++ validarfecha.inc.php
@@ -1,23 +1,24 @@
 <?php 
-
 require_once('conexion.php');
 session_start();
 	echo "<table border=".'1'." cellspacing=1 cellpadding=2 style=".'font-size: 8pt'.">";
 	echo "<tr><td>Fecha reservada</td><td>";
 	echo "Hora reservada</td><td>Peso</td><td>Talla</td><tr>";	
-	$nutri =$_SESSION['username']; 
-	$query = "select p.codU, r.fecha_reserva, r.hora_reserva 
+	//$user =$_SESSION['username']; 
+	$user="1";
+	$query = "select p.codU, r.fecha_reserva, r.hora_reserva, p.peso, p.talla 
 	from paciente p, reserva r  
-	where p.codU='$nutri'and r.codU='$nutri'
+	where p.codU='$user'and r.codU='$user'
 	order by fecha_reserva";
         $result = mysqli_query($con,$query);
   		while ($fila=mysqli_fetch_row($result)) {
 			echo "<tr>";
-			echo "<td>".$fila[0]."</td>";
 			echo "<td>".$fila[1]."</td>";
 			echo "<td>".$fila[2]."</td>";
+			echo "<td>".$fila[3]."</td>";
+			echo "<td>".$fila[4]."</td>";
+			echo "<td>".$fila[4]."</td>";
 			echo "</tr>";
 		}
-	echo "</table";
-
+	echo "</table>";
 ?>
