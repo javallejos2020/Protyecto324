5580190efa1c1cd54a1db1981765bdff02743ee6
Alvaro VP <jhonalvarovallejosperez@gmail.com>
2020-07-22 22:52:38 -0400

    Create controladorvalidarfecha.php

---
+++ controladorvalidarfecha.php
@@ -0,0 +1,21 @@
+<?php
+include "conexion.php";
+$cond=$_GET["cond"];
+$sql="select * from procesocon where codFlujo='$codFlujo' and codProceso='$codProceso'";
+$result=mysqli_query($conn,$sql);
+$fila=mysqli_fetch_array($result);
+
+if($cond=='si'or $cond=='SI'){
+	$codProceso=$fila['codProcesoSi'];
+	echo "Si".$codProceso;
+}
+else{
+	if ($cond=='no' or $cond=='NO') {
+	echo "+++++++++";
+	$codProceso=$fila['codProcesoNo'];
+	echo "No".$codProceso;
+		
+	}
+	
+}
+?>
