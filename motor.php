729ce90ee2a2141be70104c54bd7caa50bf60f4a
EnriqueCondori <obeymyrules_@hotmail.com>
2020-07-22 20:26:23 -0400

    Update motor.php

--- motor.php
+++ motor.php
@@ -6,8 +6,10 @@ include "conexion.php";
 $sql="select * from proceso where codFlujo='$codFlujo' and codproceso='$codProceso'";
 $result=mysqli_query($conn, $sql);
 $fila=mysqli_fetch_array($result);
+$tipo=$fila['tipo'];
 $codprocesosiguiente=$fila['codProcesoSiguiente'];
 $archivo=$fila['pantalla'];
+echo($archivo);
 ?>
 <body>
   <h1>Motor de Flujo</h1>
@@ -20,6 +22,7 @@ $archivo=$fila['pantalla'];
   <input type="hidden" value="<?php echo $codFlujo;?>" name="codflujo"/>
   <input type="hidden" value="<?php echo $codProceso;?>" name="codproceso"/>
   <input type="hidden" value="<?php echo $codprocesosiguiente;?>" name="codprocesosiguiente"/>
+  <input type="hidden" value="<?php echo $tipo;?>" name="tipo" />
   <input type="hidden" value="<?php echo $archivo;?>" name="archivo"/>
   <input type="submit" value="Anterior" name="Anterior"/>
   <input type="submit" value="Siguiente" name="Siguiente"/>
