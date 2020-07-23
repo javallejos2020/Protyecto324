d9eac8d6c0bd5f9a4f4c9209a926dd83a7b482a2
Alvaro VP <jhonalvarovallejosperez@gmail.com>
2020-07-22 22:52:15 -0400

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -4,5 +4,10 @@ mysqli_select_db($con,"academico_3");
 if(!$con){
 	die("No hay conexion:".mysql_connect_error());
 }
+$conn=mysqli_connect("localhost","root","");
+mysqli_select_db($conn,"flujo2");
+if(!$conn){
+	die("No hay conexion:".mysql_connect_error());
+}
 
 ?>
